0048b176af0d10e1b77d3755a1633eb34a9619fa
Aranya Bahuguna <136916046+aranya13@users.noreply.github.com>
2025-12-15 14:20:58 +0530

    Time: 1 ms (89.29%) | Memory: 45.5 MB (40.74%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        bt(res , new ArrayList<>() , nums);
+        return res;
+    }
+    public void bt(List<List<Integer>> res , List<Integer> temp , int[] nums){
+        if(temp.size() == nums.length){
+            res.add(new ArrayList<>(temp));
+        }
+        else{
+            for(int i = 0 ; i < nums.length ; i++){
+                if(temp.contains(nums[i])){
+                    continue;
+                }
+                temp.add(nums[i]);
+                bt(res , temp , nums);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+    
+}
