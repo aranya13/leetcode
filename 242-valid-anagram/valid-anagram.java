a0731ad08308106f27833433d38a121a63e04d58
Aranya Bahuguna <136916046+aranya13@users.noreply.github.com>
2025-12-23 15:55:27 +0530

    Time: 75 ms (6.19%) | Memory: 57 MB (6.88%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        String[] a = s.split("");
+        String[] b = t.split("");
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(!a[i].equals(b[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
