475ab2362ee1b3599da614ce9c577b32424b0cb6
Aranya Bahuguna <136916046+aranya13@users.noreply.github.com>
2026-01-12 10:58:38 +0530

    Time: 29 ms (42.93%) | Memory: 48.7 MB (60.06%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -3,7 +3,7 @@ public class Solution {
         for(int i = 0 ; i < nums.Length ; i++){
             for(int j = i + 1 ; j < nums.Length ; j++){
                 if(nums[i] + nums[j] == target){
-                    return new int[] {i , j};
+                    return new int[]{i , j};
                 }
             }
         }
